c705ef6c61eaa65da2dd30dc5c95ffa61e8ab590
monty <kumar.mantesh007@gmail.com>
2020-04-03 08:06:00 +0530

    commands

---
+++ cmd/subscriber/periodic.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/VISIndonesia/conveyor"
+	"github.com/VISIndonesia/conveyor/config"
+)
+
+func main() {
+	psProjID := config.Configuration.PubSub.ProjectID
+	psCredFile := config.Configuration.PubSub.CredFile
+	subID := config.Configuration.PubSub.Subscriber
+	timeout := config.Configuration.PubSub.Timeout
+
+	bqProjID := config.Configuration.BigQuery.ProjectID
+	bqCredFile := config.Configuration.BigQuery.CredFile
+	bqDataset := config.Configuration.BigQuery.Dataset
+
+	gap := config.Configuration.Gap
+
+	timeoutDuration := time.Duration(timeout) * time.Second
+
+	log.Printf("Running with configs: %+v\n", config.Configuration)
+
+	for x := range time.Tick(time.Duration(gap) * time.Second) {
+		log.Println("Starting job:", x)
+		if events, err := conveyor.Consume(subID, timeoutDuration, psProjID, psCredFile); err != nil {
+			log.Println("Error fetching messages", err)
+		} else {
+			if err := conveyor.UploadEvents(events, bqDataset, bqProjID, bqCredFile); err != nil {
+				log.Println("Error uploading data", err)
+			}
+		}
+		log.Println("run completed at:", time.Now())
+	}
+}
---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type PubSub struct {
+	CredFile   string
+	ProjectID  string
+	Subscriber string
+	Timeout    int
+}
+
+type BigQuery struct {
+	CredFile  string
+	ProjectID string
+	Dataset   string
+}
+
+type Config struct {
+	PubSub   PubSub
+	BigQuery BigQuery
+	Gap      int
+}
+
+var Configuration Config
+
+func init() {
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("$HOME/.conveyor")
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Error reading config file, %s", err)
+	}
+
+	viper.SetDefault("gap", 10)
+
+	err := viper.Unmarshal(&Configuration)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+}
